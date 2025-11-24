8c984912a89af85f6db72511add958251119b13b
Jeff Alysson <jeffalysson@gmail.com>
2025-11-24 06:47:19 -0300

    24.11.01

---
+++ js/interaction.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    let resposta = confirm("Você quer salvar as tartarugas?");
+
+    if (!resposta) {
+        
+        window.location.href = "https://www.google.com";
+    }
+};
